55de0f1c787d29918e0106edf1ac344878ac36f7
Unknown <zacharyd1@live.com>
2018-11-30 13:13:35 -0500

    [device sync] Wrote up test 1

--- IntSetTest.java
+++ IntSetTest.java
@@ -1,3 +1,8 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
 public class IntSetTest
 {
 	public static void main(String[] args)
@@ -6,9 +11,20 @@ public class IntSetTest
 	}
 	
 	
-	
+	//Tests adding a single element to the set
 	static void runTest1()
 	{
+		IntSet iSet = new IntSet(0);
+		
+		//Make sure the intSet was initialized empty
+		assertTrue(iSet.size() == 0); 
+		
+		int valToStore = 5;	
+		iSet.insert(valToStore);
+		
+		assertTrue(iSet.size() == 1);
+		assertTrue(iSet.getIndex(0) == valToStore);
+		
 		System.out.println("Test complete.");
 	}
 }
