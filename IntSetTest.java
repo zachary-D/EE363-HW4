c9c45c7791de07e7e683ca0c5b53c979257f257d
Zachary Damato <zacharyd1@live.com>
2018-11-30 13:30:02 -0500

    Updated the import commands to try to fix compile issues

--- IntSetTest.java
+++ IntSetTest.java
@@ -1,7 +1,9 @@
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertEquals;
+//import org.junit.Test;
+//import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertEquals;
+
+import IntSet.IntSet;
 
 public class IntSetTest
 {
