38e252ca080b8f26f8bf6f42e39f404a86457096
Zachary Damato <zacharyd1@live.com>
2018-11-30 16:43:08 -0500

    Updated some includes

--- IntSetTest.java
+++ IntSetTest.java
@@ -1,9 +1,14 @@
-//import org.junit.Test;
-//import static org.junit.Assert.assertTrue;
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 
-import dhouIntSet.IntSet;
+
+//import framework.Assert.assertTrue;
+//import framework.Assert.assertFalse;
+//import framework.Assert.assertEquals;
+
+import abstractions.IntSet;
 
 public class IntSetTest
 {
@@ -16,7 +21,7 @@ public class IntSetTest
 	//Tests adding a single element to the set
 	static void runTest1()
 	{
-		IntSet iSet = new IntSet(0);
+		IntSet iSet = new IntSet();
 		
 		//Make sure the intSet was initialized empty
 		assertTrue(iSet.size() == 0); 
